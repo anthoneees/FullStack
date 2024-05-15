c5053f49acffc42c92cf51e7748ba715eb83639b
ASnider <anthon.ee.es@gmail.com>
2024-05-15 02:34:11 -0400

    Finished Footer, Started creating basic about section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,76 @@ export class Footer extends Component {
   render() {
     return (
       <footer className="bg-white text-black">
-        <div className="container mx-auto py-14 px-6">footer</div>
+        <div className="outer-container w-full">
+          <div className="upper flex justify-between items-center pt-2">
+            <div>
+              <a href="/">
+                <img
+                  src="./src/assets/logo192-gold.png"
+                  alt="logo"
+                  className="h-12"
+                />
+              </a>
+            </div>
+            <div className="flex">
+              <a href="/about" className="px-4">
+                About
+              </a>
+              <a href="/" className="px-4">
+                Privacy
+              </a>
+              <a href="/" className="px-4">
+                Licensing
+              </a>
+              <a href="/" className="px-4">
+                Contact
+              </a>
+            </div>
+          </div>
+          <div className="h-0.5 mt-4 bg-slate-500"></div>
+          <div className="flex py-4">
+            <p>©2024 GMU MCVB</p>
+            <div className="ml-auto">
+              <a
+                href="https://www.instagram.com/gmumensclubvolleyball/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="./src/assets/instagram-icon-black.png"
+                  alt="Instagram"
+                  className="social-image h-6 px-2"
+                />
+              </a>
+            </div>
+            <div className="">
+              <a
+                href="https://www.gofundme.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="./src/assets/gofundme-icon-black.png"
+                  alt="GoFundMe"
+                  className="social-image h-6 ml-8 px-2"
+                />
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://mason360.gmu.edu/mensvolleyball/home/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="./src/assets/MasonM_icon-black.png"
+                  alt="Mason360"
+                  className="social-image h-6 ml-8 px-2"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
       </footer>
     );
   }
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -23,27 +23,53 @@ function LandingPage() {
               <h1 className=" text-2xl text-tertiary">
                 Welcome to Mason Men's Club Volleyball
               </h1>
-              <h2 className="text-8xl text-white">
-                George Mason's
-              </h2>
-              <h3 className="text-8xl text-white">
-                Volleyball Club
-              </h3>
+              <h2 className="text-8xl text-white">George Mason's</h2>
+              <h3 className="text-8xl text-white">Volleyball Club</h3>
               <h4 className="text-2xl text-customGray pt-8">
-              Empowering aspiring athletes through volleyball, fostering teamwork,
+                Empowering aspiring athletes through volleyball, fostering
+                teamwork,
               </h4>
               <h5 className="text-2xl text-customGray ">
-              sportsmanship, and personal growth. 
+                sportsmanship, and personal growth.
               </h5>
             </div>
           </div>
 
-          <div className=" about-section bg-white outline "> hey </div>
-          <div className=" about-section bg-secondary outline"> hey </div>
+          <div>
+            <div className=" inter-custom text-secondary py-8">
+              <h1 className ="bg-white text-4xl ml-4">About Our Program</h1>
+              <div className="h-0.5 mt-4 bg-secondary w-1/6 ml-4"></div>
+              <p className="py-8 text-xl w-5/6 ml-4">
+                We are committed to ensuring that every GMU athlete has a
+                positive experience that fuels his or her love of volleyball and
+                competition. We teach our athletes the game of volleyball
+                including all aspects of skills training, tactical knowledge,
+                match strategies as well as teaching them to be disciplined,
+                train at full effort, be a positive and supportive teammate,
+                push beyond their comfort level and compete with confidence and
+                enthusiasm.
+              </p>
+              <p className="text-xl w-5/6 ml-4">
+                The club will also provide a structure that will facilitate the
+                operation of individual teams in terms of providing practice
+                facilities, equipment, and communication. We believe all these 
+                to be necessary in delivering a safe, competitive volleyball experience 
+                for the athletes.
+              </p>
+              <a href="/events">
+                <button className="bg-secondary text-lg rounded-2xl text-white mt-8 p-1 ml-4">
+                  Ready to Play?
+                </button>
+              </a>
+            </div>
+          </div>
+          <div className="  bg-white"> </div>
           <Footer />
         </div>
 
-        <div className="bg-white h-full"></div>
+        <div className="bg-white">
+          
+        </div>
       </div>
     </>
   );
