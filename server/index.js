fbb42d7c0984e02536accc92005556fee73b1a39
ASnider <anthon.ee.es@gmail.com>
2025-05-23 19:44:57 -0400

    bug fix index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { mongoose } = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 const formsrouter = require("./routes/forms.js");
 const googlerouter = require("./routes/google.js");
