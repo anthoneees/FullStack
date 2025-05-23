d7c7aefd5b2118e6aa8411b713e336f4eac242a6
ASnider <anthon.ee.es@gmail.com>
2025-05-23 19:18:16 -0400

    Changes for Vercel Deployment

--- server/index.js
+++ server/index.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const { mongoose } = require("mongoose");
 const cors = require("cors");
-const formsrouter  = require("./routes/forms.js");
-const googlerouter = require("./routes/google.js")
-const instaFeedrouter = require("./routes/instaFeed.js")
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
+
+const formsrouter = require("./routes/forms.js");
+const googlerouter = require("./routes/google.js");
+const instaFeedrouter = require("./routes/instaFeed.js");
 
 const app = express();
+dotenv.config();
+
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
 app.use(formsrouter);
 app.use(googlerouter);
-app.use(instaFeedrouter)
+app.use(instaFeedrouter);
 
-dotenv.config()
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 const connectMongo = process.env.MONGOPASS;
@@ -27,6 +34,3 @@ mongoose
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 });
-
-
-
